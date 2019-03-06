a5f14161edb46ddbccce30064a786c7bbf6df0cb
cresiopan <cresiopan>
2019-03-06 18:35:43 -0300

    update

--- res/org-info.js
+++ res/org-info.js
@@ -1039,8 +1039,8 @@ var org_html_manager = {
             if(! t.FIXED_TOC) OrgNode.showElement(document.body);
           }
           else {
-            if(! t.VIEW_BUTTONS) OrgNode.hideElement(last_node.BUTTONS);
-            OrgNode.showElement(t.NODE.BUTTONS);
+            //if(! t.VIEW_BUTTONS) OrgNode.hideElement(last_node.BUTTONS);
+            //OrgNode.showElement(t.NODE.BUTTONS);
             t.NODE.setState(OrgNode.UNFOLDED);
             t.NODE.show();
             if(last_node.IDX != t.NODE.IDX)
